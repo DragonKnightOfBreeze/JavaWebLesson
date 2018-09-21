2bf48196c83ef868494d4b49651ffeaff01cdc79
微风的龙骑士 风游迩 <2944262241@qq.com>
2018-09-21 15:26:19 +0800

    第一单元基本学完

---
+++ src/Test/Unit_1/test8.js
@@ -0,0 +1,6 @@
+function show(id){
+    document.getElementById(id).setAttribute("style","display:block;");
+}
+function show1{
+    document.getElementById("dept").setAttribute("style","display:block;");
+}
